2e83349b416cdd2b4da889a8914821290bbce5ff
stephenfire <stephen.fire@gmail.com>
2023-11-27 18:04:29 +0800

    update to github.com/tronprotocol/protocol@GreatVoyage-v4.7.3(Chilon) upgrade required modules

--- gen-proto.sh
+++ gen-proto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INCLUDES="-I=./proto/tron -I/usr/lib -I./proto/googleapis"
-FLAGS="--go_out=./pkg/proto --go_opt paths=source_relative --go-grpc_out=./pkg/proto --go-grpc_opt=paths=source_relative"
+FLAGS="--go_opt=module=github.com/tronprotocol/grpc-gateway,paths=import --go-grpc_out=./pkg/proto --go-grpc_opt=module=github.com/tronprotocol/grpc-gateway,paths=import"
 
 protoc ${INCLUDES} ${FLAGS} ./proto/tron/core/*.proto ./proto/tron/core/contract/*.proto ./proto/tron/api/*.proto
 mkdir -p ./pkg/proto/util
